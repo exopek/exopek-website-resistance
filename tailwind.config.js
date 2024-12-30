2aaa98644003325c88ba1cb28f0740e8fead6415
jansugint <jan@exopek.de>
2024-12-31 00:17:00 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{vue,js,ts}",
+
+    "./layouts/**/*.vue",
+
+    "./pages/**/*.vue",
+
+    "./plugins/**/*.{js,ts}",
+
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
