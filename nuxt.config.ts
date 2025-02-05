fc6dafcbbb86a3e3f60a14747256b790444e025b
jansugint <jan@exopek.de>
2025-02-05 05:49:07 +0100

    chore: no default sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,19 +32,6 @@ export default defineNuxtConfig({
   }},
   sitemap: {
     urls: async () => {
-      const dynamicRoutes = ['senioren', 'fitness-einsteiger', 'kraftsportler', 'yoga', 'outdoor-fitness', 'physiotherapie', 'crossfit', 'heimtraining', 'reisen', 'rehabilitation', 'büroangestellte',
-        'pilates', 'teamsportler', 'personal-trainer', 'ausdauertraining', 'seniorensportgruppen', 'rehabilitationszentren', 'leistungssportler', 'kampfsportler', 'basketball', 'handball', 'volleyball',
-        'fussball', 'american-football', 'rugby', 'muay-thai', 'boxen', 'mma', 'hyrox', 'calisthenics', 'functional-training', 'sprungkrafttraining', 'beweglichkeitstraining', 'core-training',
-        'platzsparend', 'mobile-fitness', 'athletik-training', 'leichtathletik', 'gelenkschonend', 'klimmzüge', 'premium-widerstandsbaender-high-performance', 'trendy-widerstandsbaender-multifunktional',
-        'widerstandsbaender-gegen-plateaus', 'beste-widerstandsbaender-online-kaufen', 'widerstandsbaender-sets-alles-in-einem', 'widerstandsbaender-home-fitness', 'widerstandsbaender-mobil-und-vielseitig',
-        'widerstandsbaender-high-performance-athleten', 'widerstandsbaender-cardio-und-kraft', 'widerstandsbaender-yoga-und-pilates', 'widerstandsbaender-crossfit-training', 'widerstandsbaender-booty-workouts',
-        'widerstandsbaender-fuer-funktionssport', 'widerstandsband-sets-fuer-muskelaufbau', 'widerstandsband-sets-reha-und-fitness', 'widerstandsband-sets-home-fitness', 'premium-widerstandsband-sets', 'widerstandsband-sets-cardio-und-kraft',
-        'widerstandsband-sets-fuer-funktionssport', 'widerstandsband-sets-fuer-kleinwohnungen', 'widerstandsband-sets-fuer-neubauwohnungen', 'widerstandsband-sets-fuer-luxusfitness', 'widerstandsband-sets-fuer-personal-trainer',
-        'widerstandsband-sets-fuer-boutique-fitnessstudios', 'widerstandsband-sets-fuer-therapiezentren', 'widerstandsband-sets-fuer-ambitionierte-sportler', 'widerstandsband-sets-fuer-firmenfitness',
-        'widerstandsband-sets-fuer-luxusreisen', 'widerstandsband-sets-fuer-studioprofis', 'widerstandsband-sets-fuer-exklusiven-kraftsport', 'widerstandsband-sets-fuer-high-performance-athleten', 'widerstandsband-sets-fuer-yoga-und-pilates-studios',
-        'widerstandsband-sets-fuer-wellnesshotels', 'widerstandsband-sets-fuer-home-gym-luxus', 'widerstandsband-sets-fuer-physiotherapie-praxen', 'widerstandsband-sets-fuer-elite-sportvereine',
-        'widerstandsband-sets-fuer-exklusive-kurhotels', 'widerstandsband-sets-fuer-crossfit-boxen', 'widerstandsband-sets-fuer-luxus-gym-chains' 
-      ]
 
       
       try {
@@ -64,11 +51,7 @@ export default defineNuxtConfig({
         ];
       } catch (error) {
         console.error(error);
-        return dynamicRoutes.map(route => ({
-          changefreq: 'daily',
-          priority: 0.8,
-          loc: `https://fitnessband.exopek.de/${route}`
-        }));
+        return [];
       }
     }
   }
